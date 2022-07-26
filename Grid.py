d2a014455dfa7b35814c780a8acd843c6536a62c
t00muchstyle <you@example.com>
2022-07-26 16:43:24 +0100

    input box added to the screen

--- Grid.py
+++ Grid.py
@@ -20,6 +20,7 @@ def main():
     while True:
         drawGrid()
         draw_numbers()
+        draw_inp_box()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -37,6 +38,60 @@ def drawGrid():
             rect = pygame.Rect(x*blockSize, y*blockSize,
                                blockSize, blockSize)
             pygame.draw.rect(SCREEN, WHITE, rect, 1)
+
+
+def draw_inp_box():
+
+    font = pygame.font.Font(None, 32)
+    clock = pygame.time.Clock()
+    color_inactive = pygame.Color('lightskyblue3')
+    color_active = pygame.Color('dodgerblue2')
+    color = color_inactive
+    active = False
+    text = ''
+    done = False
+
+    input_box = pygame.Rect(400, 350,
+                               200, 50)
+    pygame.draw.rect(SCREEN, WHITE, input_box, 1)
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # If the user clicked on the input_box rect.
+                if input_box.collidepoint(event.pos):
+                    # Toggle the active variable.
+                    active = not active
+                else:
+                    active = False
+                # Change the current color of the input box.
+                color = color_active if active else color_inactive
+            if event.type == pygame.KEYDOWN:
+                if active:
+                    if event.key == pygame.K_RETURN:
+                        print(text)
+                        text = ''
+                    elif event.key == pygame.K_BACKSPACE:
+                        text = text[:-1]
+                    else:
+                        text += event.unicode
+
+        ##SCREEN.fill((30, 30, 30))
+        # Render the current text.
+        txt_surface = font.render(text, True, color)
+        # Resize the box if the text is too long.
+        width = max(200, txt_surface.get_width() + 10)
+        input_box.w = width
+        # Blit the text.
+        SCREEN.blit(txt_surface, (input_box.x + 5, input_box.y + 5))
+        # Blit the input_box rect.
+        pygame.draw.rect(SCREEN, color, input_box, 2)
+
+        pygame.display.flip()
+        clock.tick(30)
+
 
 def draw_numbers():
     font = pygame.font.SysFont('bookantigua', 28, True, False)
